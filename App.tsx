6a90fe411216d84c389f8d392fc1e584de66a593
LuizCarlosVilela <luizcarlosvilela@outlook.com.br>
2020-08-06 18:22:41 -0300

    Terminando tela inicial do App

--- App.tsx
+++ App.tsx
@@ -22,7 +22,7 @@ export default function App() {
     return (
       <>
         <Landing />
-        <StatusBar style="auto" />
+        <StatusBar style="light" />
       </>
     );
   }
--- src/pages/Landing/styles.ts
+++ src/pages/Landing/styles.ts
@@ -14,6 +14,7 @@ const styles = StyleSheet.create({
     },
 
     title: {
+        fontFamily: 'Poppins_400Regular',
         color: '#FFF',
         fontSize: 20,
         lineHeight: 30,
@@ -21,10 +22,42 @@ const styles = StyleSheet.create({
     },
 
     titleBold: {
-        fontWeight: 'bold'
+        fontFamily: 'Poppins_600SemiBold'
+    },
+
+    buttonsContainer: {
+        flexDirection: 'row',
+        marginTop: 40,
+        justifyContent: 'space-between',
+    },
+
+    button: {
+        height: 150,
+        width: '48%',
+        backgroundColor: '#333',
+        borderRadius: 8,
+        padding: 24,
+        justifyContent: 'space-between'
+    },
+    buttonPrimary: {
+        backgroundColor: '#9871f5',
+    },
+    buttonSecondary: {
+        backgroundColor: '#04d361'
+    },
+    buttonText: {
+        fontFamily: 'Archivo_700Bold',
+        color: '#FFF',
+        fontSize: 20,
+    },
+    totalConnections: {
+        fontFamily: 'Poppins_400Regular',
+        color: '#d4c2ff',
+        fontSize: 12,
+        lineHeight: 20,
+        maxWidth: 140,
+        marginTop: 40,
     }
-
-
 });
 
 export default styles;
