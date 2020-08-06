efb4053d849129b418b4da96f14aef916ba4d7ac
LuizCarlosVilela <luizcarlosvilela@outlook.com.br>
2020-08-06 16:47:32 -0300

    Fazendo a ligação para o carregamento de fontes no app

--- src/pages/Landing/styles.ts
+++ src/pages/Landing/styles.ts
@@ -1,6 +1,29 @@
 import { StyleSheet } from 'react-native';
 
 const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        backgroundColor: '#8257E5',
+        justifyContent: 'center',
+        padding: 40,
+    },
+
+    banner: {
+        width: '100%',
+        resizeMode: 'contain',
+    },
+
+    title: {
+        color: '#FFF',
+        fontSize: 20,
+        lineHeight: 30,
+        marginTop: 80,
+    },
+
+    titleBold: {
+        fontWeight: 'bold'
+    }
+
 
 });
 
