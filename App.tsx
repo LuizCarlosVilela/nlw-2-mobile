b3a8ff7e92dd8c313b2dd143e3aedb087f240345
LuizCarlosVilela <luizcarlosvilela@outlook.com.br>
2020-08-06 20:27:21 -0300

    Finalizando a tela de dar aulas para ir para a web

--- App.tsx
+++ App.tsx
@@ -6,6 +6,7 @@ import Landing from './src/pages/Landing';
 import { AppLoading } from 'expo';
 import { Archivo_400Regular, Archivo_700Bold, useFonts } from '@expo-google-fonts/archivo';
 import { Poppins_400Regular, Poppins_600SemiBold } from '@expo-google-fonts/poppins';
+import AppStack from './src/routes/AppStack';
 
 export default function App() {
   let [fontsLoaded] = useFonts({
@@ -21,7 +22,7 @@ export default function App() {
 
     return (
       <>
-        <Landing />
+        <AppStack />
         <StatusBar style="light" />
       </>
     );
---
+++ src/pages/GiveClasses/index.tsx
@@ -0,0 +1,38 @@
+import React from "react";
+import { View, ImageBackground, Text } from "react-native";
+import { useNavigation } from "@react-navigation/native";
+
+import giveClassesBackground from "../../assets/images/give-classes-background.png";
+import { RectButton } from "react-native-gesture-handler";
+
+import styles from "./styles";
+
+function GiveClasses() {
+  const { goBack } = useNavigation();
+
+  function handleNavigateBack() {
+    goBack();
+  }
+
+  return (
+    <View style={styles.container}>
+      <ImageBackground
+        resizeMode="contain"
+        source={giveClassesBackground}
+        style={styles.content}
+      >
+        <Text style={styles.title}>Quer ser um Proffy?</Text>
+        <Text style={styles.description}>
+          Para começar, você precisa se cadastrar como professor na nossa
+          plataforma web.
+        </Text>
+      </ImageBackground>
+
+      <RectButton onPress={handleNavigateBack} style={styles.okButton}>
+        <Text style={styles.okButtonText}>Tudo bem</Text>
+      </RectButton>
+    </View>
+  );
+}
+
+export default GiveClasses;
